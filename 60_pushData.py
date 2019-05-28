f54f71fa7155714040664d43acf8388b4faa2432
JimmyTinsley <liujie333333@gmail.com>
2019-05-28 09:01:33 +0800

    Add a testing plugin

---
+++ 60_pushData.py
@@ -0,0 +1,32 @@
+#!-*- coding:utf8 -*-
+
+import requests
+import time
+import json
+
+ts = int(time.time())
+payload = [
+    {
+        "endpoint": "test-endpoint",
+        "metric": "test-metric",
+        "timestamp": ts,
+        "step": 60,
+        "value": 1,
+        "counterType": "GAUGE",
+        "tags": "idc=lg,loc=beijing",
+    },
+
+    {
+        "endpoint": "test-endpoint",
+        "metric": "test-metric2",
+        "timestamp": ts,
+        "step": 60,
+        "value": 2,
+        "counterType": "GAUGE",
+        "tags": "idc=lg,loc=beijing",
+    },
+]
+
+
+if __name__ == "__main__":
+    r = requests.post("http://127.0.0.1:1988/v1/push", data=json.dumps(payload))
